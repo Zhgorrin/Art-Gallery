0598413b6a7126ea8a1ba4a26583f438d815e430
Luv Dabhi <146580963+luvdabhi@users.noreply.github.com>
2024-04-25 22:49:28 -0400

    Add files via upload
    Made add.dart functional by being able to see the image user wants to add, and also adding a comments section.

--- pj2/lib/add.dart
+++ pj2/lib/add.dart
@@ -1,12 +1,13 @@
+// add.dart
 import 'dart:io';
-
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class AddScreen extends StatefulWidget {
-  const AddScreen({super.key});
+  const AddScreen({Key? key});
 
   @override
   State<AddScreen> createState() => _AddScreenState();
@@ -15,6 +16,7 @@ class AddScreen extends StatefulWidget {
 class _AddScreenState extends State<AddScreen> {
   final TextEditingController _controllerName = TextEditingController();
   final TextEditingController _controllerQuantity = TextEditingController();
+  final TextEditingController _controllerComment = TextEditingController();
 
   GlobalKey<FormState> key = GlobalKey();
 
@@ -22,6 +24,9 @@ class _AddScreenState extends State<AddScreen> {
       FirebaseFirestore.instance.collection('shopping_list');
 
   String imageUrl = '';
+  File? imageFile;
+  double? imageWidth;
+  double? imageHeight;
 
   @override
   Widget build(BuildContext context) {
@@ -29,85 +34,140 @@ class _AddScreenState extends State<AddScreen> {
       appBar: AppBar(
         title: const Text('Add an item'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: key,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _controllerName,
-                decoration: const InputDecoration(
-                    hintText: 'Enter the name of the item'),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the item name';
-                  }
-
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _controllerQuantity,
-                decoration: const InputDecoration(
-                    hintText: 'Enter the quantity of the item'),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the item quantity';
-                  }
-
-                  return null;
-                },
-              ),
-              IconButton(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: key,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _controllerName,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter the name of the item'),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the item name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _controllerQuantity,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter the quantity of the item'),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the item quantity';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
                   onPressed: () async {
-                    ImagePicker imagePicker = ImagePicker();
-                    XFile? file =
-                        await imagePicker.pickImage(source: ImageSource.camera);
-                    print('${file?.path}');
+                    final ImagePicker _picker = ImagePicker();
+                    final XFile? pickedImage =
+                        await _picker.pickImage(source: ImageSource.gallery);
 
-                    if (file == null) return;
-                    String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
-                    Reference referenceRoot = FirebaseStorage.instance.ref();
-                    Reference referenceDirImages =
-                        referenceRoot.child('images');
+                    if (pickedImage != null) {
+                      String uniqueFileName =
+                          DateTime.now().millisecondsSinceEpoch.toString();
+                      Reference referenceRoot = FirebaseStorage.instance.ref();
+                      Reference referenceDirImages =
+                          referenceRoot.child('images');
+                      Reference referenceImageToUpload =
+                          referenceDirImages.child(uniqueFileName);
 
-                    Reference referenceImageToUpload =
-                        referenceDirImages.child('name');
+                      try {
+                        await referenceImageToUpload
+                            .putFile(File(pickedImage.path));
+                        imageUrl =
+                            await referenceImageToUpload.getDownloadURL();
 
-                    try {
-                      await referenceImageToUpload.putFile(File(file!.path));
-                      imageUrl = await referenceImageToUpload.getDownloadURL();
-                    } catch (error) {
-                      //Some error occurred
+                        ui.Codec codec = await ui.instantiateImageCodec(
+                            File(pickedImage.path).readAsBytesSync());
+                        ui.FrameInfo frameInfo = await codec.getNextFrame();
+                        setState(() {
+                          imageFile = File(pickedImage.path);
+                          imageWidth = frameInfo.image.width.toDouble();
+                          imageHeight = frameInfo.image.height.toDouble();
+                        });
+                      } catch (error) {
+                        // Handle error
+                      }
                     }
                   },
-                  icon: const Icon(Icons.camera_alt)),
-              ElevatedButton(
-                  onPressed: () async {
-                    if (imageUrl.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please upload an image')));
-                      return;
-                    }
+                  child: const Text('Pick Image'),
+                ),
+                TextFormField(
+                  controller: _controllerComment,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your comment (optional)',
+                  ),
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (key.currentState!.validate()) {
+                          String itemName = _controllerName.text;
+                          String itemQuantity = _controllerQuantity.text;
+                          String comment = _controllerComment.text;
 
-                    if (key.currentState!.validate()) {
-                      String itemName = _controllerName.text;
-                      String itemQuantity = _controllerQuantity.text;
+                          Map<String, dynamic> dataToSend = {
+                            'name': itemName,
+                            'quantity': itemQuantity,
+                            'image': imageUrl,
+                            'comment': comment,
+                          };
 
-                      Map<String, String> dataToSend = {
-                        'name': itemName,
-                        'quantity': itemQuantity,
-                        'image': imageUrl,
-                      };
-
-                      _reference.add(dataToSend);
-                    }
-                  },
-                  child: const Text('Submit'))
-            ],
+                          _reference.add(dataToSend);
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ],
+                ),
+                if (imageFile != null)
+                  GestureDetector(
+                    onTap: () {
+                      _showImageDialog(context, imageFile!.path);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: double.infinity,
+                      child: AspectRatio(
+                        aspectRatio: imageWidth! / imageHeight!,
+                        child: Image.file(
+                          imageFile!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  void _showImageDialog(BuildContext context, String imagePath) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Image.file(
+              File(imagePath),
+              fit: BoxFit.contain,
+            ),
+          ),
+        );
+      },
     );
   }
 }
