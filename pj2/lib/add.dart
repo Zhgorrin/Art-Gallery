e35914a2ee2de3fb54933d6387a70faf3de6326d
Zhgorrin <53023370+Zhgorrin@users.noreply.github.com>
2024-04-27 17:36:31 -0400

    Minor UI Fix

--- pj2/lib/add.dart
+++ pj2/lib/add.dart
@@ -53,10 +53,10 @@ class _AddScreenState extends State<AddScreen> {
                 TextFormField(
                   controller: _controllerQuantity,
                   decoration:
-                      const InputDecoration(hintText: 'Enter the quantity'),
+                      const InputDecoration(hintText: 'Enter the metadata'),
                   validator: (String? value) {
                     if (value == null || value.isEmpty) {
-                      return 'Please enter a quantity';
+                      return 'Please enter the metadata';
                     }
                     return null;
                   },
